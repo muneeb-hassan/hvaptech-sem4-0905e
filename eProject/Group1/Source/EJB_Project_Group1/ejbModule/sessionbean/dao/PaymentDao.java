98cdc2210b3df0a19ba69ffa62ea4e39d6dec978
quoctoantruong1987 <quoctoantruong1987@gmail.com>
2011-06-24 09:43:42 +0000

    update them 1 so function cho cac Dao va them 1 so Dao moi

---
+++ eProject/Group1/Source/EJB_Project_Group1/ejbModule/sessionbean/dao/PaymentDao.java
@@ -0,0 +1,18 @@
+package sessionbean.dao;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class PaymentDao
+ */
+@Stateless
+public class PaymentDao implements PaymentDaoRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public PaymentDao() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
